a8bc4626ae185cd7f0bf9533e9fc2c6343f81244
dumbugg1312 <brandon.ballard@hotmail.com>
2026-06-23 10:28:04 -0500

    Update calendar — Jun 23 (add Heated Rivalry 6/27, Songwriter Rounds 6/28)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='diypensacola-278ffd5c6739';
+var CACHE='diypensacola-845eda1efbab';
 var CORE=['./','index.html','logo.png','manifest.webmanifest','icon-192.png','icon-512.png','apple-touch-icon.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
