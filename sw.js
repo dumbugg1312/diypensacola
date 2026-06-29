f0b4f1a7425a7f61bcaa5ed6330e864d388298ed
dumbugg1312 <brandon.ballard@hotmail.com>
2026-06-29 15:40:14 -0500

    w

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='diypensacola-84580416a4e8';
+var CACHE='diypensacola-be44d9298e13';
 var CORE=['./','index.html','logo.png','manifest.webmanifest','icon-192.png','icon-512.png','apple-touch-icon.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
