cadb7a81ac3e91867002a0eaae3bff54389ff40a
dumbugg1312 <brandon.ballard@hotmail.com>
2026-07-05 23:12:50 -0500

    archive backfill: +10 shows with flyers (IG history Aug-Dec 2024) + touring band origins

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='diypensacola-3174e4c20220';
+var CACHE='diypensacola-ed3b7710fa56';
 var CORE=['./','index.html','offline.html','logo.png','manifest.webmanifest','icon-192.png','icon-512.png','apple-touch-icon.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
