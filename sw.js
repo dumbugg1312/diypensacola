413fa6bfc59431f28d35ebb68d66f2129ce93241
dumbugg1312 <brandon.ballard@hotmail.com>
2026-07-06 16:14:55 -0500

    archive backfill: +27 shows with flyers (IG history Dec 11 2022 through Jan 2023)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='diypensacola-fcfb37d67822';
+var CACHE='diypensacola-cff6277d6dbb';
 var CORE=['./','index.html','offline.html','logo.png','manifest.webmanifest','icon-192.png','icon-512.png','apple-touch-icon.png'];
 self.addEventListener('install',function(e){
   self.skipWaiting();
